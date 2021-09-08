95fe670a2c3c5270eb384fd3b2aaa423d6243108
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-09-08 20:09:48 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,14 +1,11 @@
-export interface AddSurveyModel {
-  question: string
-  answers: SurveyAnswer[]
-  date: Date
-}
-
-export interface SurveyAnswer {
-  image?: string
-  answer: string
-}
+import { SurveyAnswerModel } from '../models/survey'
 
 export interface AddSurvey {
   add: (data: AddSurveyModel) => Promise<void>
+}
+
+export interface AddSurveyModel {
+  question: string
+  answers: SurveyAnswerModel[]
+  date: Date
 }
