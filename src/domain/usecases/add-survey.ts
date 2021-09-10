d8484755e1e6467634a2c7127d8266a5fa33b24f
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-09-09 21:48:19 -0300

    refactor: change models to use type alias instead of interfaces

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,11 +1,11 @@
 import { SurveyAnswerModel } from '@/domain/models/survey'
 
-export interface AddSurvey {
-  add: (data: AddSurveyModel) => Promise<void>
-}
-
-export interface AddSurveyModel {
+export type AddSurveyModel = {
   question: string
   answers: SurveyAnswerModel[]
   date: Date
+}
+
+export interface AddSurvey {
+  add: (data: AddSurveyModel) => Promise<void>
 }
