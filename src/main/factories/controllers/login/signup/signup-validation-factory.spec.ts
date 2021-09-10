29d06db8798535bdb058ab46898df2d3e43fad84
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-09-09 22:33:21 -0300

    refactor: improve paths with modules

--- src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
@@ -3,7 +3,7 @@ import { EmailValidator } from '@/validation/protocols/email-validator'
 import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
 import { Validation } from '@/presentation/protocols/validation'
 
-jest.mock('../../../../../validation/validators/validation-composite')
+jest.mock('@/validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
