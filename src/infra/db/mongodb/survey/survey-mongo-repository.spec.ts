b1720d7ad8ff6e8ec0d2dcca66eec28139e53fda
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-09-12 18:37:17 -0300

    test: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -77,4 +77,22 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const result = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer'
+          }
+        ],
+        date: new Date()
+      })
+      const sut = makeSut()
+      const survey = await sut.loadById(result.insertedId.toHexString())
+      expect(survey).toBeTruthy()
+    })
+  })
 })
