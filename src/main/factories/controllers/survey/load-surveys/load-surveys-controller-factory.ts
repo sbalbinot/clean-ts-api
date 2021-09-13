a91bc3bdcef5ec527cc4d3b7d6c674925839c905
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-09-12 23:50:02 -0300

    refactor: change file name

--- src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -1,4 +1,4 @@
-import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys.factory'
+import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { Controller } from '@/presentation/protocols'
 import { LoadSurveysController } from '@/presentation/controllers/survey/load-surveys/load-surveys-controller'
