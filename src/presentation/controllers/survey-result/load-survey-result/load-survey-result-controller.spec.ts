dc6c9622ab53611ae674fac3920ae9441705a1d6
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-09-21 18:22:13 -0300

    refactor: change SurveyResult to return isCurrentAccountAnswer

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -8,6 +8,7 @@ import MockDate from 'mockdate'
 import faker from 'faker'
 
 const mockRequest = (): HttpRequest => ({
+  accountId: faker.datatype.uuid(),
   params: {
     surveyId: faker.datatype.uuid()
   }
@@ -60,10 +61,11 @@ describe('LoadSurveyResultController', () => {
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
-  test('Should call LoadSurveyResult with correct value', async () => {
+  test('Should call LoadSurveyResult with correct values', async () => {
     const { sut, loadSurveyResultSpy } = makeSut()
     const httpRequest = mockRequest()
     await sut.handle(httpRequest)
+    expect(loadSurveyResultSpy.accountId).toBe(httpRequest.accountId)
     expect(loadSurveyResultSpy.surveyId).toBe(httpRequest.params.surveyId)
   })
 
