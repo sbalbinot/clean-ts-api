f9e3c78306ccedad12503917476c7746ab3a0c19
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-10-20 20:45:44 -0300

    chore: update libs

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,11 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import * as path from 'path'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(path.join(__dirname, '/../routes')).map(async file => {
+  readdirSync(join(__dirname, '/../routes')).map(async file => {
     if (!file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
